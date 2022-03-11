da4a6dc047d2e2e70b6998246f5d2cacde421103
mithlesh kumar <mithlesh.9m@gmail.com>
2022-03-11 13:39:06 +0530

    changes code fizzbuzz

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -4,33 +4,35 @@ describe Solver do
   context 'When testing the Solver class' do
     solver = Solver.new
 
-    it 'the method factorial should return the factorial of given integer' do
+    it 'the method fizzbuzz should return "fizz"' do
+      value = 3
+      10.times do |_i|
+        if value % 5 != 0
+          result = solver.fizzbuzz(value)
+          expect(result).to eq 'fizz'
+        end
+        value += 3
+      end
+    end
+
+    it 'the method fizzbuzz should return "buzz"' do
       value = 5
-      result = solver.factorial(value)
-      expect(result).to eq 120
+      10.times do |_i|
+        if value % 3 != 0
+          result = solver.fizzbuzz(value)
+          expect(result).to eq 'buzz'
+        end
+        value += 5
+      end
     end
 
-    it 'the method factorial should return the factorial of given integer' do
-      value = 4
-      result = solver.factorial(value)
-      expect(result).to eq 24
-    end
-
-    it 'the method factorial should return the factorial of given integer' do
-      value = 0
-      result = solver.factorial(value)
-      expect(result).to eq 1
-    end
-
-    it 'the method factorial should return the factorial of given integer' do
-      value = 1
-      result = solver.factorial(value)
-      expect(result).to eq 1
-    end
-
-    it 'the method factorial should throw an exception for negative integer' do
-      value = -2
-      expect { solver.factorial(value) }.to raise_error(StandardError)
+    it 'the method fizzbuzz should return "fizzbuzz"' do
+      value = 15
+      10.times do |_i|
+        result = solver.fizzbuzz(value)
+        expect(result).to eq 'fizzbuzz'
+        value += 15
+      end
     end
   end
 end
